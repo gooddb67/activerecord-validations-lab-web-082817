dabaf96e9314acd33e33b2877016ad5654e868d7
Dave Goodman <davegoodman20@gmail.com>
2017-09-25 11:05:18 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: :true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :validate_title
+
+
+  def validate_title
+    phrases = ["Won't Believe", "Secret", "Top [number]}", "Guess"]
+    if self.title
+
+      phrases.each do |phrase|
+        if self.title.include?(phrase)
+          return
+        end
+      end
+      self.errors[:title] << "Need a title with a clickbait-y title"
+    end
+  end
+
+
+
 end
